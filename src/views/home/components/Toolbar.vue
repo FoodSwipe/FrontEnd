9a67b7f7ee1d370e057f8cab7504cb10bd894759
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-12-11 01:51:31 +0545

    Added mainframeBB font

--- src/views/home/components/Toolbar.vue
+++ src/views/home/components/Toolbar.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-app-bar
 		color="#FFC107"
-		height="60"
+		height="58"
 		width="100vw"
 		class="ma-0 pr-4"
 	>
@@ -11,107 +11,115 @@
 		<v-spacer />
 		<v-tooltip bottom>
 			<template #activator="{on, attrs}">
-				<v-btn
-					v-show="$route.name !== 'Food Swipe'"
-					icon
-					small
-					class="pr-0 mr-sm-6 mr-md-6 mr-lg-6 mr-xl-6"
-					v-bind="attrs"
-					@click="toHome()"
-					v-on="on"
-				>
-					<v-icon :size="
-						$vuetify.breakpoint.width > 300
-							? ''
-							: '16'
-					"
+				<v-scale-transition>
+					<v-btn
+						v-show="$route.name !== 'Food Swipe'"
+						icon
+						small
+						class="pr-0 mr-sm-6 mr-md-6 mr-lg-6 mr-xl-6"
+						v-bind="attrs"
+						@click="toHome()"
+						v-on="on"
 					>
-						home
-					</v-icon>
-				</v-btn>
+						<v-icon :size="
+							$vuetify.breakpoint.width > 300
+								? ''
+								: '16'
+						"
+						>
+							home
+						</v-icon>
+					</v-btn>
+				</v-scale-transition>
 			</template>
 			<span>Home</span>
 		</v-tooltip>
 		<v-tooltip bottom>
 			<template #activator="{on, attrs}">
-				<v-btn
-					v-show="$route.name !== 'Profile'"
-					icon
-					small
-					class="pr-0 mr-sm-6 mr-md-6 mr-lg-6 mr-xl-6 profile-avatar"
-					v-bind="attrs"
-					@click="toProfile()"
-					v-on="on"
-				>
-					<v-icon :size="
-						$vuetify.breakpoint.width > 300
-							? ''
-							: '16'
-					"
+				<v-scale-transition>
+					<v-btn
+						v-show="$route.name !== 'Profile'"
+						icon
+						small
+						class="pr-0 mr-sm-6 mr-md-6 mr-lg-6 mr-xl-6 profile-avatar"
+						v-bind="attrs"
+						@click="toProfile()"
+						v-on="on"
 					>
-						account_circle
-					</v-icon>
-				</v-btn>
+						<v-icon :size="
+							$vuetify.breakpoint.width > 300
+								? ''
+								: '16'
+						"
+						>
+							account_circle
+						</v-icon>
+					</v-btn>
+				</v-scale-transition>
 			</template>
 			<span>Kiran Parajuli</span>
 		</v-tooltip>
 		<v-tooltip bottom>
 			<template #activator="{on, attrs}">
-				<v-btn
-					v-show="$route.name !== 'Store'"
-					icon
-					small
-					class="pr-0 mr-sm-6 mr-md-6 mr-lg-6 mr-xl-6"
-					v-bind="attrs"
-					@click="toStore()"
-					v-on="on"
-				>
-					<v-icon :size="
-						$vuetify.breakpoint.width > 300
-							? ''
-							: '16'
-					"
+				<v-slide-x-transition>
+					<v-btn
+						v-show="$route.name !== 'Store'"
+						icon
+						small
+						class="pr-0 mr-sm-6 mr-md-6 mr-lg-6 mr-xl-6"
+						v-bind="attrs"
+						@click="toStore()"
+						v-on="on"
 					>
-						store
-					</v-icon>
-				</v-btn>
+						<v-icon :size="
+							$vuetify.breakpoint.width > 300
+								? ''
+								: '16'
+						"
+						>
+							store
+						</v-icon>
+					</v-btn>
+				</v-slide-x-transition>
 			</template>
 			<span>Store</span>
 		</v-tooltip>
 
-		<v-badge
-			v-show="$route.name !== 'Cart'"
-			dark
-			color="black"
-			content="5"
-			offset-x="10"
-			offset-y="15"
-		>
-			<v-tooltip bottom>
-				<template #activator="{on, attrs}">
-					<v-btn
-						light
-						icon
-						small
-						class="mr-2"
-						v-bind="attrs"
-						@click="toCart()"
-						v-on="on"
-					>
-						<v-icon
-							:size="
-								$vuetify.breakpoint.width > 300
-									? ''
-									: '16'
-							"
+		<v-fade-transition>
+			<v-badge
+				v-show="$route.name !== 'Cart'"
+				dark
+				color="black"
+				content="5"
+				offset-x="10"
+				offset-y="15"
+			>
+				<v-tooltip bottom>
+					<template #activator="{on, attrs}">
+						<v-btn
+							light
+							icon
+							small
+							class="mr-2"
+							v-bind="attrs"
+							@click="toCart()"
+							v-on="on"
 						>
-							add_shopping_cart
-						</v-icon>
-					</v-btn>
-				</template>
-				<span>Cart</span>
-			</v-tooltip>
-		</v-badge>
+							<v-icon
+								:size="
+									$vuetify.breakpoint.width > 300
+										? ''
+										: '16'
+								"
+							>
+								add_shopping_cart
+							</v-icon>
+						</v-btn>
+					</template>
+					<span>Cart</span>
+				</v-tooltip>
+			</v-badge>
+		</v-fade-transition>
 	</v-app-bar>
 </template>
 
@@ -153,20 +161,24 @@ export default {
 }
 </style>
 <style lang="sass" scoped>
+@font-face
+	font-family: "MainframeBB"
+	src: url("../../../../src/assets/MAINBRG_.TTF")
 .organization-title
 	transition: all .3s
-	font-size: 2.5rem
-	line-height: 2.6rem
-	font-family: 'Sacramento', cursive
+	text-transform: uppercase
+	font-size: 1.5rem
+	line-height: 1.5rem
+	font-family: MainframeBB, serif
 	@media only screen and (max-width: 600px)
-		font-size: 2rem
-		line-height: 2rem
-	@media only screen and (max-width: 300px)
-		font-size: 1.6rem
-		line-height: 1.6rem
-	@media only screen and (max-width: 185px)
-		font-size: 1.2rem
-		line-height: 1.2rem
+		font-size: 1.3rem
+		line-height: 1.3rem
+	@media only screen and (max-width: 320px)
+		font-size: 1.1rem
+		line-height: 1.1rem
+	@media only screen and (max-width: 220px)
+		font-size: .8rem
+		line-height: .8rem
 .profile-avatar
 	cursor: pointer
 	border: 2px solid white
