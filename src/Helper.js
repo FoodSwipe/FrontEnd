464071116e77aa750dc022a4632789540127f867
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-12-19 22:37:42 +0545

    Removed redundant methods and optimized used methods

--- src/Helper.js
+++ src/Helper.js
@@ -1,6 +1,5 @@
 const keysToIgnore = [
 	"id",
-	"imageForUpload",
 	"created_by",
 	"updated_by",
 	"approved_by",
@@ -9,62 +8,19 @@ const keysToIgnore = [
 	"updated_at"
 ]
 
-const requiredObjectsForUser = ["profile"]
-
 module.exports = {
-	/**
-	 * Cooks create data for a model
-	 *
-	 * @param {string} imageKey name of image key
-	 * @param {Object} rawData editedItem to be parsed
-	 *
-	 * @return {Object}
-	 */
-	cookCreateData({ imageKey= "", rawData= {} }) {
-		// remove image from object if not added
-		if (rawData.imageForUpload !== undefined) {
-			rawData[imageKey] = rawData.imageForUpload[0]
-		} else delete rawData[imageKey]
-		return rawData
-	},
-	/**
-	 * Cooks edit data for a model
-	 * @param {string} target model
-	 * @param {Object} rawData editedItem to be parsed
-	 * @param {string} imageKey name of image key
-	 *
-	 * @return {Object}
-	 */
-	cookEditData({ target= "", rawData= {}, imageKey= "" }) {
-		const body = {}
-		let objArray = []
-		// // get required objects to be id'ed
-		// if (target === "branch") objArray = requiredObjectsForBranch
-		// else if (target === "event") objArray = requiredObjectsForEvent
-		// // get id from objects
-		// objArray.forEach(model => {
-		// 	if (rawData[model] !== undefined && rawData[model] !== null) {
-		// 		if (typeof rawData[model] === "object") {
-		// 			body[model] = rawData[model].id
-		// 		}
-		// 	}
-		// })
-		// think about image field
-		if (rawData.imageForUpload !== undefined) {
-			body[imageKey] = rawData.imageForUpload[0]
-		}
-
-		// copy entire rawData to a separate body
-		for (const [key, value] of Object.entries(rawData)) {
-			if(!body.hasOwnProperty(key)) {
-				body[key] = value
-			}
-		}
-		// remove image from object if not changed
-		if (rawData.imageForUpload !== undefined) {
-			body[imageKey] = rawData.imageForUpload[0]
-		} else delete body[imageKey]
-		return body
+	cookEditData(targetData, to_strip_fields_array) {
+		to_strip_fields_array.forEach((field) => {
+			const datum = targetData[field]
+			delete targetData[field]
+			if (Array.isArray(datum)) {
+				targetData[field] = []
+				datum.forEach((data) => {
+					targetData[field].push(data.id)
+				})
+			} else targetData[field] = datum.id
+		})
+		return targetData
 	},
 	/**
 	 * Return form data for provided raw object
