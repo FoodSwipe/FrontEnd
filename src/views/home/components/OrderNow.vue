ad942c56cebc9898168d64ba59c1686e575e11ff
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-22 22:10:25 +0545

    Show on going order details

--- src/views/home/components/OrderNow.vue
+++ src/views/home/components/OrderNow.vue
@@ -3,7 +3,7 @@
 		flat
 		tile
 		color="transparent"
-		class="px-2"
+		class="px-4"
 	>
 		<v-carousel :show-arrows="false"
 			height="100"
@@ -25,12 +25,13 @@
 			</v-carousel-item>
 		</v-carousel>
 		<v-card
+			v-if="!isDisabled"
 			color="#ffeebcdb"
 			rounded
 			max-width="820"
 			class="mx-auto"
 		>
-			<v-row class="ma-0 pa-1"
+			<v-row class="ma-0 pa-0"
 				justify="center"
 				align="center"
 			>
@@ -95,15 +96,150 @@
 				</v-col>
 			</v-row>
 		</v-card>
+		<v-card v-else
+			flat
+			rounded
+			max-width="820"
+			class="mx-auto"
+		>
+			<v-card-title class="d-flex justify-space-between align-center">
+				<div>
+					You have an ongoing order.
+				</div>
+				<v-btn icon
+					to="/cart"
+				>
+					<v-icon>shopping_cart</v-icon>
+				</v-btn>
+			</v-card-title>
+			<v-divider />
+			<v-card-text v-if="pendingOrder"
+				class="pt-1"
+			>
+				<div class="pa-2 d-flex align-center flex-wrap">
+					<v-chip class="ml-1 mb-2">
+						<v-icon size="20">
+							person_pin_circle
+						</v-icon>
+						<div class="px-1">
+							{{ pendingOrder.custom_location }}
+						</div>
+					</v-chip>
+					<v-chip class="ml-1 mb-2">
+						<v-icon size="20">
+							phone
+						</v-icon>
+						<div class="px-1">
+							{{ pendingOrder.custom_contact }}
+						</div>
+					</v-chip>
+					<v-chip v-if="pendingOrder.custom_email"
+						class="ml-1 mb-2"
+					>
+						<v-icon size="20">
+							email
+						</v-icon>
+						<div class="px-1">
+							{{ pendingOrder.custom_email }}
+						</div>
+					</v-chip>
+					<v-chip class="ml-1 mb-2">
+						<div class="px-1">
+							<span><b>Rs</b></span><span class="number-font">{{ pendingOrder.total_price }}</span>
+						</div>
+						<span>
+							Total
+						</span>
+					</v-chip>
+				</div>
+				<div v-if="$vuetify.breakpoint.width > 600" class="pr-16"
+					style="position: relative;"
+				>
+					<div class="d-flex align-center flex-wrap">
+						<div v-if="pendingOrder['cart_items'].length > 0"
+							class="d-flex align-center flex-wrap"
+						>
+							<v-card v-for="item in pendingOrder['cart_items']"
+								:key="item.id"
+								class="pa-2 ml-2 mb-1 text-center"
+								color="grey darken-3"
+							>
+								<v-avatar>
+									<v-img :src="item.item.image" />
+								</v-avatar>
+								<div class="subtitle-2 cursor"
+									@click="$router.push({ name: 'Product', params: { id: item.item.id }})"
+								>
+									{{ item.item.name }}
+								</div>
+								<div class="point875">
+									Quantity: <v-chip small
+										class="number-font"
+									>
+										{{ item.quantity }}
+									</v-chip>
+								</div>
+							</v-card>
+						</div>
+						<div v-else>
+							<div class="px-2">
+								<v-btn to="/store">
+									<v-icon size="16">
+										fastfood
+									</v-icon>
+									<span v-if="$vuetify.breakpoint.smAndUp"
+										style="font-size: 12px;"
+										class="pl-2"
+									>Start order</span>
+								</v-btn>
+							</div>
+						</div>
+					</div>
+					<v-tooltip bottom>
+						<template #activator="{on, attrs}">
+							<v-btn
+								style="position: absolute; right: 1%; top: 0;"
+								:icon="$vuetify.breakpoint.width < 600"
+								v-bind="attrs"
+								v-on="on"
+							>
+								<v-icon size="16">
+									shopping_basket
+								</v-icon>
+							</v-btn>
+						</template>
+						<span>Checkout</span>
+					</v-tooltip>
+				</div>
+				<div v-else>
+					<v-btn v-if="pendingOrder['cart_items'].length === 0"
+						to="/store"
+						block
+					>
+						<v-icon size="16">
+							fastfood
+						</v-icon>
+					</v-btn>
+					<v-btn
+						v-else
+						block
+						@click="routeToOrderConfirmation()"
+					>
+						Checkout
+					</v-btn>
+				</div>
+			</v-card-text>
+		</v-card>
 	</v-card>
 </template>
 <script>
-import { refineOrderNowList } from "@/Helper"
 import { mapGetters } from "vuex"
 import router from "@/router"
+import Snack from "@/mixin/Snack"
 
 export default {
 	name: "OrderNowComponent",
+	mixins: [Snack],
 	data: () => ({
 		itemsFieldRequiredErrorMessage: null,
 		isDisabled: false,
@@ -120,6 +256,7 @@ export default {
 		}),
 	},
 	async created() {
+		console.log(this.pendingOrder)
 		this.$bus.on("refresh-order-now", this.preFillForm)
 
 		await this.initialize()
@@ -129,6 +266,9 @@ export default {
 		this.$bus.off("refresh-order-now", this.preFillForm)
 	},
 	methods: {
+		routeToOrderConfirmation() {
+			router.push({name: "Confirm Order"})
+		},
 		async initialize() {
 			this.isDisabled = (this.$helper.getCookingOrderId() !== null)
 		},
@@ -145,11 +285,6 @@ export default {
 					custom_contact: ""
 				}
 			}
-		},
-		async openSnack(text, color="error") {
-			await this.$store.dispatch("snack/setSnackState", true)
-			await this.$store.dispatch("snack/setSnackColor", color)
-			await this.$store.dispatch("snack/setSnackText", text)
 		},
 		async startShopping() {
 			const started = await this.$store.dispatch("order/startOrder", this.order)
