046c0bcd5b9b55dea4824998488260a472df0169
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-25 05:11:22 +0545

    Used fetched detail rather setters thru emits

--- src/components/CartIndicator.vue
+++ src/components/CartIndicator.vue
@@ -31,7 +31,6 @@
 
 <script>
 import { mapGetters } from "vuex"
-import router from "@/router"
 
 export default {
 	name: "CartIndicator",
@@ -41,52 +40,15 @@ export default {
 			default: true
 		}
 	},
-	data: () => ({
-		currentUser: null,
-		cartCount: "0",
-	}),
 	computed: {
 		...mapGetters({
 			alreadyCookingOrder: "order/detailOrder"
 		}),
-	},
-	async created() {
-		this.$bus.on("set-cart-count", this.setCartCount)
-		this.$bus.on("add-cart-count-by-one", this.addCartCountByOne)
-		this.$bus.on("subtract-cart-count", this.subtractCartCount)
-		this.$bus.on("update-cart-count", this.updateCartCount)
-		await this.initialize()
-	},
-	beforeUnmount() {
-		this.$bus.off("set-cart-count", this.setCartCount)
-		this.$bus.off("add-cart-count-by-one", this.addCartCountByOne)
-		this.$bus.off("subtract-cart-count", this.subtractCartCount)
-		this.$bus.on("update-cart-count", this.updateCartCount)
-	},
-	methods: {
-		updateCartCount(expression) {
-			this.cartCount = (parseInt(this.cartCount) + expression).toString()
-		},
-		subtractCartCount(value) {
-			this.cartCount = (parseInt(this.cartCount) - parseInt(value)).toString()
-		},
-		addCartCountByOne() {
-			this.cartCount = (parseInt(this.cartCount) + 1).toString()
-		},
-		setCartCount(count) {
-			this.cartCount = count.toString()
-		},
-		async initialize() {
-			this.currentUser = this.$helper.getCurrentUser()
-			this.showAdminButton = this.$helper.isAdminUser()
-			const cookingOrder = this.$helper.getCookingOrderId()
-			if (cookingOrder) {
-				await this.$store.dispatch("order/withCartItems", {
-					id: cookingOrder
-				})
-				this.cartCount = this.alreadyCookingOrder.total_items.toString()
-			}
-		},
+		cartCount() {
+			if (this.alreadyCookingOrder) {
+				return this.alreadyCookingOrder["total_items"]
+			} else return 0
+		}
 	},
 }
 </script>
