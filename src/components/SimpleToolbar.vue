61749d327efa90305542cb0a7d7392e2e92f5efa
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-25 05:09:16 +0545

    Added router links

--- src/components/SimpleToolbar.vue
+++ src/components/SimpleToolbar.vue
@@ -1,8 +1,39 @@
 <template>
-	<v-toolbar flat>
-		<div class="organization-title">
-			Food swipe
-		</div>
+	<v-toolbar flat
+		style="border-bottom: 2px solid darkorange;"
+	>
+		<v-btn text
+			to="/"
+		>
+			<div class="organization-title">
+				Food swipe
+			</div>
+		</v-btn>
+		<v-spacer />
+		<v-btn to="/store"
+			text
+			class="ma-1"
+		>
+			<v-icon size="16">
+				store
+			</v-icon>
+			<span
+				v-if="$vuetify.breakpoint.width > 300"
+				style="font-size: 12px;"
+			>Store</span>
+		</v-btn>
+		<v-btn to="/cart"
+			text
+			class="ma-1"
+		>
+			<v-icon size="16">
+				shopping_cart
+			</v-icon>
+			<span
+				v-if="$vuetify.breakpoint.width > 300"
+				style="font-size: 12px;"
+			>Cart</span>
+		</v-btn>
 	</v-toolbar>
 </template>
 
