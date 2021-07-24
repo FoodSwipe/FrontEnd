6ca7f133560480d44965311f1a5646b0464dfd93
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-24 22:30:53 +0545

    Added scroll top component

---
+++ src/components/ScrollTop.vue
@@ -0,0 +1,46 @@
+<template>
+	<v-fab-transition>
+		<v-btn
+			v-show="scrollTop"
+			v-scroll="onScroll"
+			color="pink"
+			fab
+			dark
+			small
+			fixed
+			bottom
+			right
+			@click="scrollToTop"
+		>
+			<v-icon>arrow_drop_up</v-icon>
+		</v-btn>
+	</v-fab-transition>
+</template>
+
+<script>
+export default {
+	name: "ScrollTop",
+	props: {},
+	data: () => ({
+		scrollTop: null
+	}),
+	computed: {},
+	methods: {
+		scrollToTop() {
+			window.scroll({
+				top: 0,
+				behavior: "smooth"
+			})
+		},
+		onScroll(e) {
+			if (typeof window === "undefined") return
+			const top = window.pageYOffset || e.target.scrollTop
+			this.scrollTop = top > 150
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
