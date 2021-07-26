8d72ff3ad8cc7ae389036a0751016fa6da5b2c09
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-26 21:40:50 +0545

    Added flikity swiper on home top image

--- src/views/home/components/Swiper.vue
+++ src/views/home/components/Swiper.vue
@@ -1,39 +1,58 @@
 <template>
-	<v-card :loading="loading"
+	<v-card
+		dark
+		:loading="loading"
 		height="100vh"
 		tile
+		style="position: relative"
 	>
-		<v-img
-			v-if="homePageContents"
-			:src="homePageContents[0].image"
-			dark
-			height="100vh"
-			style="position: relative"
+		<flickity ref="flickity"
+			:options="flickityOptions"
 		>
-			<home-toolbar style="position: absolute; top:0;" />
-			<order-now style="position: absolute; top: 30vh;" />
-			<div
-				style="position: absolute; top: 90vh; width: 100vw; display: flex; justify-content: center"
+			<v-card
+				v-for="item in homePageContents"
+				:key="item.id"
+				flat
+				tile
+				class="carousel-cell"
+				height="100vh"
 			>
-				<v-btn icon
-					large
-					@click="scrollDown"
-				>
-					<v-icon x-large>
-						expand_more
-					</v-icon>
-				</v-btn>
-			</div>
-		</v-img>
+				<v-img
+					eager
+					class="carousel-image"
+					:src="item.image"
+					dark
+					gradient="to top, rgb(0 0 0 / 0%), rgb(0 0 0 / 60%), rgb(0 0 0 / 20%)"
+					height="100vh"
+				/>
+			</v-card>
+		</flickity>
+		<home-toolbar style="position: absolute; top:0;" />
+		<order-now style="position: absolute; top: 30vh;" />
+		<div
+			style="position: absolute; top: 90vh; width: 100vw; display: flex; justify-content: center"
+		>
+			<v-btn icon
+				large
+				@click="scrollDown"
+			>
+				<v-icon x-large>
+					expand_more
+				</v-icon>
+			</v-btn>
+		</div>
 	</v-card>
 </template>
 <script>
 import { mapGetters } from "vuex"
 import Snack from "@/mixin/Snack"
+import Flickity from "vue-flickity"
+
 
 export default {
 	name: "ShowCaseSlider",
 	components: {
+		Flickity,
 		HomeToolbar: () => import("@/views/home/components/Toolbar"),
 		OrderNow: () => import("@/views/home/components/OrderNow"),
 	},
@@ -46,6 +65,16 @@ export default {
 			"blue-gradient",
 			"green-gradient-rgba"
 		],
+		flickityOptions: {
+			initialIndex: 0,
+			autoPlay: 6000,
+			prevNextButtons: false,
+			pauseAutoPlayOnHover: true,
+			pageDots: false,
+			wrapAround: true,
+			selectedAttraction: 0.01,
+			friction: 0.25,
+		},
 	}),
 	computed: {
 		...mapGetters({
@@ -59,6 +88,7 @@ export default {
 		async initialize() {
 			this.isLoading = true
 			const fetched = await this.$store.dispatch("homePageContent/fetchAllHomePageContent")
+			console.log(this.homePageContents)
 			if (!fetched) {
 				await this.openSnack("Internal server error. Please try again")
 			} else { this.isLoading = false}
@@ -72,3 +102,16 @@ export default {
 	}
 }
 </script>
+
+<style lang="scss" scoped>
+.carousel-cell {
+	display: block;
+	height: 40vh;
+	width: 100vw;
+}
+.carousel-image {
+	object-fit: cover;
+	width: 100%;
+	height: 100%;
+}
+</style>
