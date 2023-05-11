acad34fb527960a199d0fb25b460d6ed166195bd
Parajuli Kiran <kiranparajuli589@gmail.com>
2023-05-11 14:33:59 +0545

    Removed non required files

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	facebookUrl: "https://www.facebook.com/foodswipeonline",
-	instagramUrl: "https://www.instagram.com/foodswipeonlinefood/?hl=en",
-	LOYALTY_DISCOUNT: 10,
-	DELIVERY_START_PM: 17,
-	DELIVERY_START_AM: 4,
-	DELIVERY_CHARGE: 50,
-	LOYALTY_STARTS_AT: 10000,
-}
